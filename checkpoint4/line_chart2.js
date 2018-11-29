acc0557c19f3d480ceb1679fcd05f1aed13de00e
Miguel Regouga <migasreg@me.com>
2018-11-29 12:41:02 +0000

    Fucking corrigido

--- checkpoint4/line_chart2.js
+++ checkpoint4/line_chart2.js
@@ -1,103 +1,57 @@
-var margin = {top: 50, right: 50, bottom: 50, left: 50}
-  , width = (window.innerWidth - margin.left - margin.right)/2 // Use the window's width 
-  , height = (window.innerHeight - margin.top - margin.bottom)/2; // Use the window's height
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-// The number of datapoints
-var n = 21;
+// parse the date / time
+var parseTime = d3.timeParse("%d/%m/%y");
 
-// 5. X scale will use the index of our data
-var xScale = d3.scaleLinear()
-    .range([0, width]); // output
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-// 6. Y scale will use the randomly generate number 
-var yScale = d3.scaleLinear() 
-    .range([height, 0]); // output 
-console.log("line_chart eheh");
-console.log(selec_countries);
-// 7. d3's line generator
-var line = d3.line()
-    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-    .curve(d3.curveMonotoneX) // apply smoothing to the line
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
 
-//var xAxis = d3.axisBottom(x).tickFormat(d3.format("d")), 
-//yAxis = d3.axisLeft(y).ticks(5),
-
-//generateLine = d3.line().x(function(d){return x(d.Date);}).y(function(d){return y(d.Streams)});
-// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-var dataset = d3.range(n).map(function(d) { 
-  return {"y": d3.csv("Dataset.csv", function(data) {
-  
-  console.log(data[0]);
-  //data.filter(function(d) { return d.Country == "ar" });
-  //data.filter(function(d) { return d.Country == "ar" });
-}) } 
-})
-
-// 1. Add the SVG to the page and employ #2
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-// 3. Call the x axis in a group tag
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+// Get the data
+d3.csv("js/data.csv", function(error, data) {
+  if (error) throw error;
 
-// 4. Call the y axis in a group tag
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  });
 
-// 9. Append the path, bind the data, and call the line generator 
-svg.append("path")
-    .datum(dataset) // 10. Binds data to the line 
-    .attr("class", "line") // Assign a class for styling 
-    .attr("d", line); // 11. Calls the line generator 
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
-// 12. Appends a circle for each datapoint 
-svg.selectAll(".dot")
-    .data(dataset)
-  .enter().append("circle") // Uses the enter().append() method
-    .attr("class", "dot") // Assign a class for styling
-    .attr("cx", function(d, i) { return xScale(i) })
-    .attr("cy", function(d) { return yScale(d.y) })
-    .attr("r", 5)
-      .on("mouseover", function(a, b, c) { 
-  			console.log(a) 
-        this.attr('class', 'focus')
-		})
-      .on("mouseout", function() {  })
-//       .on("mousemove", mousemove);
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
 
-//   var focus = svg.append("g")
-//       .attr("class", "focus")
-//       .style("display", "none");
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-//   focus.append("circle")
-//       .attr("r", 4.5);
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-//   focus.append("text")
-//       .attr("x", 9)
-//       .attr("dy", ".35em");
-
-//   svg.append("rect")
-//       .attr("class", "overlay")
-//       .attr("width", width)
-//       .attr("height", height)
-//       .on("mouseover", function() { focus.style("display", null); })
-//       .on("mouseout", function() { focus.style("display", "none"); })
-//       .on("mousemove", mousemove);
-  
-//   function mousemove() {
-//     var x0 = x.invert(d3.mouse(this)[0]),
-//         i = bisectDate(data, x0, 1),
-//         d0 = data[i - 1],
-//         d1 = data[i],
-//         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-//     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-//     focus.select("text").text(d);
-//   }
+});
